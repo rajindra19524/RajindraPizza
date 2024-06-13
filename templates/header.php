2853b3679865b0685ea3978fda6d1c1116bbb882
Rajindra Pitigalaarachchi <95735946+rajindra19524@users.noreply.github.com>
2024-06-13 11:23:48 +0530

    Add header

---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Ninja Pizza</title>
+    <style>
+        .brand{
+            background:#cbb09c !important;
+        }
+
+        .brand-text{
+            color:#cbb09c !important;
+        }
+    </style>
+</head>
+
+<body class="indigo lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="center brand-logo brand-text">Rajindra Pizza</a>
+
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add a pizza</a></li>
+            </ul>
+        </div>    
+    </nav?
