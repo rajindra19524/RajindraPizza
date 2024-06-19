d5bf939bde1a3c678c24c9e9221dc761681f9030
Rajindra Pitigalaarachchi <95735946+rajindra19524@users.noreply.github.com>
2024-06-19 21:51:14 +0530

    fetch data

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
 
     //connect to database
-    $conn = mysqli_connect('localhost','shaun','test1234','rajindra_pizza');
+    $conn = mysqli_connect('localhost','root','','rajindra_pizza');
 
     //check connection
    if(!$conn){
         echo 'connection error '.mysqli_connect_error();
     }
+    
+   //write query for all pizzas
+   $sql = "SELECT id,title,ingrediants FROM pizzas";
+
+   //make query and get result
+   $result = mysqli_query($conn,$sql);
+
+   //fetch the resulting rows as an array
+   $pizzas = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+   //free result from memory
+   mysqli_free_result($result);
+
+   //close the connection
+   mysqli_close($conn);
+
+   print_r($pizzas);
 
 
 
